cabfdf97e16b44aeee3287c8dd67de34fa0cd335
John Ramsden <johnramsden@users.noreply.github.com>
2015-06-22 17:51:59 -0700

    Added neccesary (unfinished) functions. Background shows up.

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
 }
 
 // Update the enemy's position, required method for game
@@ -24,13 +25,27 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    
+}
 
+Player.prototype.update = function(dt) {
+    
+}
+
+Player.prototype.render = function() {
+    
+}
+
+Player.prototype.handleInput = function(allowedKeys) {
+    
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var allEnemies = [];
+var player = new Player;
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
