08ae84802f64402c4a8e459f88210dbd1a30b4c7
John Ramsden <johnramsden@users.noreply.github.com>
2015-06-24 00:33:04 -0700

    Randomized start location and speed of enemy every update.

--- js/app.js
+++ js/app.js
@@ -1,23 +1,18 @@
+var randomBetweenInterval = function(start, end) {
+        return Math.floor((Math.random() * (end-start+1)) + start);
+}
+
 // Constants
 var BLOCK_WIDTH = 101;
 var BLOCK_HEIGHT = 83;
-var MAX_SPEED = 100;    // Max speed in pixels
+var MAX_SPEED = 200;    // Max speed in pixels
+var MIN_SPEED = 50;          // Min speed in pixels
 var OFFSET = 20;        // Offset to have bug in center of square
-var NUM_ENEMIES = 10;   // Total number of Enemies
+var NUM_ENEMIES = 5;   // Total number of Enemies
 var CANVAS_WIDTH = 505;
     
     // Enemies our player must avoid
-var Enemy = function() {
-   
-    // Returns a multiple of row height for rock textures
-    var randomRockRow = function() {
-        return (Math.floor((Math.random() * 3) + 1) * BLOCK_HEIGHT) - OFFSET;
-    }
-    
-    var randomSpeed = function() {
-        return Math.floor((Math.random() * MAX_SPEED) + 1);
-    }
-       
+var Enemy = function() {       
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -25,30 +20,45 @@ var Enemy = function() {
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     
-    // Start off screen
-    this.x = -BLOCK_WIDTH;
+    // Start random distance off screen up to a screen off
+    this.x = this.randomStartPos();
     
     // In order to choose which row to start the enemy 
     // we choose a random row from 1 to 3 where 1 is 
     // top rock square and 3 is bottom rock square and
     // multiply by row height.
-    this.y = randomRockRow();
+    this.y = this.randomRockRow();
     
-    this.speed = randomSpeed();
+    this.speed = this.randomSpeed();
 }
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     this.x += this.speed * dt;
-    if(this.x > CANVAS_WIDTH){
-        this.x = -BLOCK_WIDTH;
+    if(this.x > CANVAS_WIDTH){ // Enemy at end of screen
+        this.y = this.randomRockRow();
+        this.speed = this.randomSpeed();
+        this.x = -BLOCK_WIDTH; 
     }
 }
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+    // Returns a multiple of row height for rock textures
+Enemy.prototype.randomRockRow = function() {
+        return (randomBetweenInterval(1, 3) * BLOCK_HEIGHT) - OFFSET;
+}
+    
+Enemy.prototype.randomSpeed = function() {
+        return randomBetweenInterval(MIN_SPEED, MAX_SPEED);
+}
+
+Enemy.prototype.randomStartPos = function() {
+        return -randomBetweenInterval(0, CANVAS_WIDTH);
 }
 
 // Now write your own player class
